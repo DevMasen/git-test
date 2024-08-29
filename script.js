6a929d95f7a8fcd05934a0f8ad56b6164e004ef6
M Hosein <49146950+DevMasen@users.noreply.github.com>
2024-08-29 16:43:57 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const btn = document.querySelector(".btn");
 
 const randInt = function (start = 0, end = 0) {
 	return Math.floor(Math.random() * (Math.abs(end - start) + 1) + start);
-	// [0,1) =>*(end-start+1) = [0, end-start+1) =>+start = [start, end+1)=> floor() = [start, end]
 };
 
-const generateColor = function () {
-	return `rgb(${randInt(0,255)},${randInt(0,255)},${randInt(0,255)})`;
-};
+const generateColor = () => `rgb(${randInt(0,255)},${randInt(0,255)},${randInt(0,255)})`;
+
 
 btn.addEventListener("click", () => {
     const randomColor = generateColor();
