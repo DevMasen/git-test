73ef25c75989961b54e5e8397a144f36a3aad70a
M Hosein <49146950+DevMasen@users.noreply.github.com>
2024-08-29 16:52:42 +0330

    refactor: randInt function changed

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const btn = document.querySelector(".btn");
 
-const randInt = function (start = 0, end = 0) {
-	return Math.floor(Math.random() * (Math.abs(end - start) + 1) + start);
-};
+const randInt = (start = 0, end = 0) => Math.floor(Math.random() * (Math.abs(end - start) + 1) + start);
 
 const generateColor = () => `rgb(${randInt(0,255)},${randInt(0,255)},${randInt(0,255)})`;
-
 
 btn.addEventListener("click", () => {
     const randomColor = generateColor();
